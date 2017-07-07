239d74d6194e4e422ad546725cc8f78ca9a7abcc
David Hey <davidhey@me.com>
2017-07-07 16:43:14 -0400

    label clusters, drop ID features

--- tracklist_pandas.py
+++ tracklist_pandas.py
@@ -14,7 +14,7 @@ df_artist_piv = df.pivot_table(values='track', columns='artist_basic',
 print "shape before:"
 print df_artist_piv.shape
 df_artist_piv[df_artist_piv > 0] = 1
-df_artist_piv = df_artist_piv.loc[:, (df_artist_piv.sum() > 4)]
+df_artist_piv = df_artist_piv.loc[:, (df_artist_piv.sum() > 7)]
 print "shape after:"
 print df_artist_piv.shape
 
@@ -33,6 +33,8 @@ print df_piv.shape
 
 #join tracks and artists features
 df_m=df_piv.merge(df_artist_piv, how='left', left_index=True, right_index=True)
+#drop ID columns
+del df_m['ID']
 
 #create clusters
 n_clusters = 3
@@ -40,8 +42,22 @@ kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init=1).fit(df_m)
 df_m['cluster'] = kmeans.labels_
 print df_m['cluster']
 
+#label clusters
+terms = df_m.columns.values
+order_centroids = kmeans.cluster_centers_.argsort()[:,::-1]
+df_m['label'] = "None"
+for i in range(n_clusters):
+	label = None
+	for ind in order_centroids[i, :5]:
+		if label is None:
+			label = terms[ind]
+		else:
+			label += ", " + terms[ind]
+	df_m.loc[df_m.cluster == i, ['label']] = label 
+
 #how many artists per cluster
-print df_m.groupby('cluster').size()
+df_m['label_new'] = df_m['cluster'].map(str) + ' - ' + df_m['label']
+print df_m.groupby('label_new').size()
 
 df.set_index('artist', inplace=True)  #simplifies joins later
 
